30697e7b9aed957ebe0317c8f5d3640379f77fb4
Zalaya <enol.corzal@gmail.com>
2025-04-09 22:41:00 +0200

    feat: add the new directories for the application and structure the gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,29 @@ plugins {
     id("java")
 }
 
-group = "dev.zalaya"
+group = "dev.zalaya.overkill"
 version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+dependencies {
+    // JUnit
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,1 @@
-rootProject.name = "overkill"
-
+rootProject.name = "parent"
