14f8b5b4483c89d6159a69291f49ada560c5a37a
Zalaya <enol.corzal@gmail.com>
2025-04-16 19:28:06 +0200

    fix: solve a problem with the task mapper mappings ignoring some attributes

--- backend/src/main/java/dev/zalaya/overkill/backend/infrastructure/persistence/mapper/TaskEntityMapper.java
+++ backend/src/main/java/dev/zalaya/overkill/backend/infrastructure/persistence/mapper/TaskEntityMapper.java
@@ -4,6 +4,7 @@ import dev.zalaya.overkill.backend.domain.model.Task;
 import dev.zalaya.overkill.backend.infrastructure.persistence.entity.TaskEntity;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 import java.util.List;
@@ -14,6 +15,9 @@ public interface TaskEntityMapper {
     Task toDomain(TaskEntity taskEntity);
     List<Task> toDomain(List<TaskEntity> taskEntities);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     TaskEntity toEntity(Task task);
     List<TaskEntity> toEntity(List<Task> tasks);
 
